6338810aba328a53a9f5e051226159046f32f9d7
clpowe <clpowe@gmail.com>
2025-05-20 20:49:01 -0400

    animation fix

--- layers/ui/components/AnimatedNumber.vue
+++ layers/ui/components/AnimatedNumber.vue
@@ -1,47 +1,51 @@
 <script setup lang="ts">
 
+const props = defineProps<{ value: number | string }>()
 
-const props = defineProps<{
-  value: number | string
-}>()
+let current = props.value
+let previous = props.value
+const showCurrent = ref(true)
 
-const displayed = ref(props.value)
-const numberRef = ref<HTMLElement | null>(null)
-let tl: gsap.core.Timeline
+const currentRef = ref<HTMLElement | null>(null)
+const previousRef = ref<HTMLElement | null>(null)
 
 watch(
   () => props.value,
-  async (newVal) => {
-    await nextTick()
-
-    if (!numberRef.value) return
-    if (tl) tl.kill()
-
-    tl = useGSAP().timeline()
+  async (oldVal, newVal) => {
+    console.log(oldVal, newVal)
+    showCurrent.value = false
+    const tl = useGSAP().timeline()
 
     // Animate old value out
-    tl.to(numberRef.value, {
+    tl.to(previousRef.value, {
       y: -30,
       autoAlpha: 0,
       duration: 0.3,
       ease: 'power2.in',
       onComplete: () => {
-        displayed.value = newVal
+        showCurrent.value = true
       },
     })
-
     // Animate new value in
     tl.fromTo(
-      numberRef.value,
+      currentRef.value,
       { y: 30, autoAlpha: 0 },
       { y: 0, autoAlpha: 1, duration: 0.3, ease: 'power2.out' }
     )
+
+    previous = props.value
+    current = props.value
   }
 )
 </script>
 
 <template>
-  <span ref="numberRef" class="inline-block">
-    {{ displayed }}
+  <span class="inline-block relative h-[1em] w-auto">
+    <span ref="previousRef" class="absolute left-0 top-0 w-full">
+      {{ previous }}
+    </span>
+    <span ref="currentRef" class="absolute left-0 top-0 w-full">
+      {{ current }}
+    </span>
   </span>
 </template>
